6383a4434183aecf2e271fc1822750308bda295e
maqsatto <m.syrtbay@outlook.com>
2026-01-12 21:10:14 +0500

    update: ExistsByEmail method added

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -3,26 +3,45 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"time"
 
 	"github.com/maqsatto/Notes-API/internal/domain"
 )
 
-type userRepo struct {
-	ctx context.Context
-	db  *sql.DB
+type UserRepo struct {
+	db *sql.DB
 }
 
-func NewUserRepo(db *sql.DB) *userRepo {
-	return &userRepo{
-		ctx: context.Background(),
-		db:  db,
+func NewUserRepo(db *sql.DB) *UserRepo {
+	return &UserRepo{
+		db: db,
 	}
 }
+func (r *UserRepo) ExistsByEmail(ctx context.Context, email string) (bool, error) {
+	query := `SELECT EXISTS(SELECT 1 FROM users WHERE email = $1)`
+	var exists bool
+	err := r.db.QueryRowContext(ctx, query, email).Scan(&exists)
+	if err != nil {
+		return false, err
+	}
 
-func (r *userRepo) CreateUser(user *domain.User) error {
-	query := `INSERT INTO users (username, email, password, created_at, updated_at, deleted_at) VALUES ($1, $2, $3, $4, $5, $6)`
-	timeDateNow := time.Now()
-	r.db.QueryRow(query, &user.Username, &user.Username, &user.Password, timeDateNow, timeDateNow, "NULL")
+	return exists, err
+}
+func (r *UserRepo) CreateUser(ctx context.Context, user *domain.User) error {
+	exists, err := r.ExistsByEmail(ctx, user.Email)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("email already exists")
+	}
+	query := `INSERT INTO users 
+    				(username, email, password, created_at, updated_at, deleted_at) 
+			  VALUES ($1, $2, $3, $4, $5, $6)`
+	now := time.Now()
+	if _, err := r.db.ExecContext(ctx, query, &user.Username, &user.Email, &user.Password, now, now, nil); err != nil {
+		return err
+	}
 	return nil
 }
