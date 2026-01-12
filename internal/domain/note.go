90d179e2cb5af1b67ebcc4e423afdd9106bb7df7
maqsatto <m.syrtbay@outlook.com>
2026-01-12 20:02:57 +0500

    add: Domain models

--- internal/domain/note.go
+++ internal/domain/note.go
@@ -1,1 +1,14 @@
 package domain
+
+import "time"
+
+type Note struct {
+	ID        uint64    `json:"id"`
+	UserID    uint64    `json:"user_id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+	Tags      []string  `json:"tags"`
+}
--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,1 +1,12 @@
 package domain
+
+import "time"
+
+type User struct {
+	ID        uint64    `json:"id"`
+	Email     string    `json:"email"`
+	Username  string    `json:"username"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
