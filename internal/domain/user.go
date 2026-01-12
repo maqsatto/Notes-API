cf97bb667b3210364de81ed4f63bdd04e2f4b29f
maqsatto <m.syrtbay@outlook.com>
2026-01-12 20:40:06 +0500

    fix: added password json security

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -6,6 +6,7 @@ type User struct {
 	ID        uint64    `json:"id"`
 	Email     string    `json:"email"`
 	Username  string    `json:"username"`
+	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	DeletedAt time.Time `json:"deleted_at"`
