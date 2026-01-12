92f5c66348eca7ed5d19ef7462daf9ca5c93e2ff
Bauka <baykatv5@gmail.com>
2026-01-12 18:46:24 +0500

    added config db

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,69 @@
 package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	Database DatabaseConfig
+}
+type DatabaseConfig struct {
+    Host     string
+    Port     string
+    User     string
+    Password string
+    DBName   string
+    SSLMode  string
+}
+
+func Load() (*Config, error) {
+	cfg := &Config{
+		Database: DatabaseConfig{
+			Host: getEnv("DB_HOST", "localhost"),
+			Port: getEnv("DB_PORT", "5432"),
+			User: getEnv("DB_USER", "postgres"),
+			Password: getEnv("DB_PASSWORD", ""),
+			DBName: getEnv("DB_NAME", ""),
+			SSLMode: getEnv("DB_SSLMODE", "disable"),
+		},
+	}
+	if err := cfg.Validate(); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
+func (c *Config) Validate() error {
+    if c.Database.Password == "" {
+        return fmt.Errorf("DB_PASSWORD is required")
+    }
+    // if c.JWT.Secret == "" {
+    //     return fmt.Errorf("JWT_SECRET is required")
+    // }
+    return nil
+}
+
+func (d DatabaseConfig) ConnectionString() string {
+    return fmt.Sprintf(
+        "postgresql://%s:%s@%s:%s/%s?sslmode=%s",
+        d.User, d.Password, d.Host, d.Port, d.DBName, d.SSLMode,
+    )
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
+func getEnvAsInt(key string, defaultVal int) int {
+    valueStr := getEnv(key, "")
+    if value, err := strconv.Atoi(valueStr); err == nil {
+        return value
+    }
+    return defaultVal
+}
+
--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -1,2 +1,48 @@
 package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
 // DB connection setup
+
+type Config struct {
+	Host string
+	Port string
+	User string
+	Password string
+	DBName string
+	SSLMode string
+}
+
+func NewPostgresDB(ctx context.Context ,cfg Config) (*sql.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user%s password%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode,
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("error opening database: %w", err)
+	}
+
+	// Connection pool settings
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(5 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("error connecting to database: %w", err)
+	}
+
+	return db, nil
+}
