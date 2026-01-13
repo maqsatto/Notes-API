751e9cce477d1e7ca3c59db3821dadae62a18d39
Bauka <baykatv5@gmail.com>
2026-01-13 09:28:18 +0500

    add: Utils: readjson, writejson

--- internal/http/router/router.go
+++ internal/http/router/router.go
@@ -11,18 +11,13 @@ import (
 type Deps struct {
 	Config *config.Config
 	Logger *logger.Logger
-	DB     any // replace with your real db type (*sql.DB or *sqlx.DB)
+	DB     any
 }
 
 func New(d Deps) http.Handler {
 	mux := http.NewServeMux()
 
-	// register routes here (health, notes, users...)
-	// mux.HandleFunc("/health", ...)
 
-	// wrap with middleware here (recovery, logger, cors...)
-	// handler := middleware.Recovery(d.Logger)(mux)
-	// handler = middleware.Logger(d.Logger)(handler)
-
-	return mux
+	//USERS
+	mux.Handle("GET /api/users", handler)
 }
--- internal/utils/json.go
+++ internal/utils/json.go
@@ -1,2 +1,30 @@
 package utils
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		WriteJSON(w, 400, "Bad json")
+		return
+	}
+}
+
+func ReadJSON(r *http.Request, req any) error {
+	defer r.Body.Close()
+	d := json.NewDecoder(r.Body)
+	d.DisallowUnknownFields()
+	if err := d.Decode(req); err != nil {
+		return err
+	}
+	if err := d.Decode(&struct{}{}); err != io.EOF {
+		return fmt.Errorf("multiple json values")
+	}
+	return nil
+}
