a800c771a5a734bc2555a62c3b819a41e6fe6e47
Bauka <baykatv5@gmail.com>
2026-01-13 08:49:00 +0500

    add: logger, recovery

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,1 +1,47 @@
 package logger
+
+import (
+	"log"
+	"os"
+)
+
+type Logger struct {
+	info *log.Logger
+	error *log.Logger
+	file *os.File
+}
+
+func New() (*Logger, error) {
+	// create logs directory if not exists
+	if err := os.MkdirAll("logs", 0755); err != nil {
+		return nil, err
+	}
+
+	file, err := os.OpenFile(
+		"logs/app.text",
+		os.O_CREATE|os.O_WRONLY|os.O_APPEND,
+		0644,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Logger{
+		info: log.New(os.Stdout, "[INFO] ", log.Ldate|log.Ltime|log.Lshortfile),
+		error: log.New(os.Stderr, "[ERROR] ", log.Ldate|log.Ltime|log.Lshortfile),
+		file: file,
+	}, nil
+}
+
+func (l *Logger) Info(msg string) {
+	l.info.Println(msg)
+}
+
+func (l *Logger) Error(msg string, err any) {
+	l.error.Printf("$s: %v\n", msg, err)
+}
+
+func (l *Logger) Close() error {
+	return l.file.Close()
+}
--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -21,12 +21,12 @@ func NewUserRepo(db *sql.DB) *UserRepo {
 func (r *UserRepo) ExistsByEmail(ctx context.Context, email string) (bool, error) {
 	query := `SELECT EXISTS(SELECT 1 FROM users WHERE email = $1)`
 	var exists bool
-	err := r.db.QueryRowContext(ctx, query, email).Scan(&exists)
-	if err != nil {
+
+	if err := r.db.QueryRowContext(ctx, query, email).Scan(&exists); err != nil {
 		return false, err
 	}
 
-	return exists, err
+	return exists, nil
 }
 func (r *UserRepo) CreateUser(ctx context.Context, user *domain.User) error {
 	exists, err := r.ExistsByEmail(ctx, user.Email)
@@ -36,8 +36,8 @@ func (r *UserRepo) CreateUser(ctx context.Context, user *domain.User) error {
 	if exists {
 		return fmt.Errorf("email already exists")
 	}
-	query := `INSERT INTO users 
-    				(username, email, password, created_at, updated_at, deleted_at) 
+	query := `INSERT INTO users
+    				(username, email, password, created_at, updated_at, deleted_at)
 			  VALUES ($1, $2, $3, $4, $5, $6)`
 	now := time.Now()
 	if _, err := r.db.ExecContext(ctx, query, &user.Username, &user.Email, &user.Password, now, now, nil); err != nil {
