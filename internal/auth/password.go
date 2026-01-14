6ab189b953a2866688b1abc0551afd28faa53a11
Bauka <baykatv5@gmail.com>
2026-01-14 15:44:30 +0500

    add: hash and check password

--- internal/auth/password.go
+++ internal/auth/password.go
@@ -1,3 +1,24 @@
 package auth
 
+import "golang.org/x/crypto/bcrypt"
+
 //Bcrypt hashing
+
+func HashPassword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword(
+		[]byte(password),
+		bcrypt.DefaultCost,
+	)
+	if err != nil {
+		return "", nil
+	}
+	return string(hashed), nil
+}
+
+func CheckPassword(hashedPassword, password string) bool {
+	err := bcrypt.CompareHashAndPassword(
+		[]byte(hashedPassword),
+		[]byte(password),
+	)
+	return err == nil
+}
