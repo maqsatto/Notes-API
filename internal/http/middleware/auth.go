0bfe5ef8564b84d7862f2ad209078ca74dd0647d
Bauka <baykatv5@gmail.com>
2026-01-14 16:30:57 +0500

    add: auth middleware

--- internal/http/middleware/auth.go
+++ internal/http/middleware/auth.go
@@ -1,1 +1,53 @@
 package middleware
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/maqsatto/Notes-API/internal/auth"
+)
+
+type ctxKey string
+
+const userIDKey ctxKey = "user_id"
+
+func UserIDFromContext(ctx context.Context) (uint64, bool) {
+	v := ctx.Value(userIDKey)
+	id, ok := v.(uint64)
+	return id, ok
+}
+
+func AuthMiddleware(jwtm *auth.JWTManager) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			h := r.Header.Get("Authorization")
+			if h == "" {
+				http.Error(w, "missing Authorization header", http.StatusUnauthorized)
+				return
+			}
+
+			parts := strings.SplitN(h, " ", 2)
+			if len(parts) != 2 || !strings.EqualFold(parts[0], "Bearer") {
+				http.Error(w, "invalid Authorization header format", http.StatusUnauthorized)
+				return
+			}
+
+			tokenString := strings.TrimSpace(parts[1])
+			if tokenString == "" {
+				http.Error(w, "empty token", http.StatusUnauthorized)
+				return
+			}
+
+			claims, err := jwtm.ParseAndValidate(tokenString)
+			if err != nil {
+				http.Error(w, "invalid or expired token", http.StatusUnauthorized)
+				return
+			}
+
+			ctx := context.WithValue(r.Context(), userIDKey, claims.UserID)
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
